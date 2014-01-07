541a872b051574a03e57d002f6f502452dcdde3a
TimEvWw <tim.evers@gmail.com>
2014-01-07 22:20:25 +0000

    moving files into directories, adding home command to menu

---
+++ lib/controller.rb
@@ -0,0 +1,55 @@
+# FarmBot Controller
+
+#require "Rubygems"
+#require "FarmBotDbAccess"
+#require "./hardware/ramps.rb"
+
+require './lib/controlcommand.rb'
+require './lib/filehandler.rb'
+
+# This module ties the different components for the FarmBot together
+# It reads the next action from the database, executes it and reports back
+# and it will initiate the synchronization with the web systems
+
+class Control
+
+	def initialize	
+		@inactiveCounter 	= 0
+	end
+	
+	def runCycle
+
+		if $command_queue.empty? == false
+
+			command = $command_queue.pop
+			command.lines.each do |command_line|
+				case command_line.action.upcase
+					when "MOVE ABSOLUTE"
+						$bot_hardware.moveAbsolute(command_line.xCoord, command_line.yCoord, command_line.zCoord)
+					when "MOVE RELATIVE"
+						$bot_hardware.moveRelative(command_line.xCoord, command_line.yCoord, command_line.zCoord)
+					when "HOME X"
+						$bot_hardware.moveHomeX
+					when "HOME Y"
+						$bot_hardware.moveHomeY
+					when "HOME Z"
+						$bot_hardware.moveHomeZ
+					when "SET SPEED"
+						$bot_hardware.setSpeed(command_line.speed)
+					when "SHUTDOWN"
+						puts "shutdown"
+						$shutdown = 1
+				end
+
+			end			
+			#$commandFinished << command if command.commandid != nil and command.commandid > 0
+		else
+			sleep 0.1
+		end
+		
+	end
+end
+
+#$bot_hardware 		= FarmBotControlInterface.new
+#$command_queue		= Queue.new
+#$command_finished	= Queue.new
